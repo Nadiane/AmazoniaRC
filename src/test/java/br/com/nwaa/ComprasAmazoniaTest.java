21f605eb9581ea128871118c0aa6708da3cc369c
Nadiane Torres França Ferreira <nadiane.t.ferreira@accenture.com>
2019-12-03 22:27:13 -0300

    Classe de teste.

--- src/test/java/br/com/nwaa/ComprasAmazoniaTest.java
+++ src/test/java/br/com/nwaa/ComprasAmazoniaTest.java
@@ -2,12 +2,13 @@ package br.com.nwaa;
 
 import br.com.nwaa.entidades.Cliente;
 import br.com.nwaa.entidades.Compra;
+import br.com.nwaa.entidades.Produto;
 import junit.framework.TestCase;
 import org.jetbrains.annotations.TestOnly;
 
 public class ComprasAmazoniaTest extends TestCase {
 
-    public void pesquisarCliente(){
+   /* public void pesquisarCliente(){
         Cliente cliente = ComprasAmazoniaFachada.pesquisarCliente("01234567890");
         assertTrue(cliente.getCpf().equals("01234567890"));
     }
@@ -16,6 +17,22 @@ public class ComprasAmazoniaTest extends TestCase {
         Cliente cliente = ComprasAmazoniaFachada.pesquisarCliente("32145609871");
         assertTrue(cliente == null);
     }
+
+    public void pesquisarClienteSemPassarCpf(){
+        Cliente cliente = ComprasAmazoniaFachada.pesquisarCliente("");
+        assertTrue(cliente == null);
+    }
+
+    public void pesquisarProduto(){
+        Produto produto =  ComprasAmazoniaFachada.pesquisarProduto("P0001");
+        assertTrue(produto.getCodigo().equals("P0001"));
+    }
+
+    public void pesquisarProdutoSemPassarCodigo(){
+        Produto produto =  ComprasAmazoniaFachada.pesquisarProduto("");
+        assertTrue(produto == null);
+    }
+*/
 
 
 }
